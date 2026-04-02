d1037930062901724f4b47669b7a535a94731455
Gaston <rivatagaston@gmail.com>
2026-04-01 23:58:15 -0300

    primer push

---
+++ Controllers/ArtemisController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Net.Http;
+using System.Text.Json;
+
+
+namespace ArtemisTracker.Controllers
+{
+    
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArtemisController : ControllerBase
+    {
+        static int baseDistance = 10000;
+        static DateTime start = DateTime.UtcNow;
+        [HttpGet("distance")]
+        public IActionResult GetDistance()
+        {
+            var seconds = (DateTime.UtcNow - start).TotalSeconds;
+
+            var distance = baseDistance + (seconds * 1500); // velocidad fake
+
+            return Ok(new
+            {
+                distance = (int)distance
+            });
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Servicios
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// 👇 esto es clave para que veas algo en /
+app.MapGet("/index.html", () => "API Artemis funcionando 🚀");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.Run();
