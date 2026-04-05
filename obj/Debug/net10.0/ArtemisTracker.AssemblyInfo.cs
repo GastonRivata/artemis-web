e244636e39851ce165ad94f047d0415a5833e84f
Gaston <rivatagaston@gmail.com>
2026-04-05 00:33:29 -0300

    Cambio hud,backroung y mas

--- obj/Debug/net10.0/ArtemisTracker.AssemblyInfo.cs
+++ obj/Debug/net10.0/ArtemisTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArtemisTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d4eee6097940157b9125ed94fbb3af038fa83c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26292f5841dd30c60d56e56e625c4f83ee0a0683")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArtemisTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArtemisTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
